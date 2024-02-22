68e7b70af6253078a300c46eefdf28965f8c784b
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-21 20:18:16 -0800

    Moved code from play.js into input.js

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
-
-const client = require("./client");
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  // your code here
-
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-client.connect();
+connect();
 setupInput();
