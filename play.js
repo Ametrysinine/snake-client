ac8d3c91fbd5b29ee9054474d665fe57a2b887eb
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-21 20:11:21 -0800

    Added stdin to play.js

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
 
 const client = require("./client");
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // your code here
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting ...");
 client.connect();
+setupInput();
