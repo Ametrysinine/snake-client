2984a5fde865f0a674fc7b248f6e5434d181d833
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-02-21 20:49:12 -0800

    Added snake movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,6 +12,21 @@ const setupInput = function () {
 
 const handleUserInput = function(key) {
   // your code here
+  if (key === 'w' || key === 'W' || key === '\033[A') {
+    connection.write('Move: up');
+  }
+
+  if (key === 'a' || key === 'A'|| key === '\033[D') {
+    connection.write('Move: left');
+  }
+
+  if (key === 's' || key === 'S'|| key === '\033[B') {
+    connection.write('Move: down');
+  }
+
+  if (key === 'd' || key === 'D'|| key === '\033[C') {
+    connection.write('Move: right');
+  }
 
   if (key === '\u0003') {
     process.exit();
